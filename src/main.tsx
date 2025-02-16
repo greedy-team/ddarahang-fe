0d0d5d055a6cccf66a896a1f00294298c96fbea7
songhj10207@naver.com <songhj10207@naver.com>
2025-02-16 16:18:26 +0900

    feature: MSW 라이브리 설치 및 기본 세팅 #1

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { worker } from './mocks/browser.ts';
 import App from './App.tsx';
+
+worker.start();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
