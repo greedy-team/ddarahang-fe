8cdc1f61ff23f723f378c49e2bc2f9ac4d199f21
gogo1414 <wnstn6945@gmail.com>
2025-02-18 20:07:27 +0900

    fix: MSW 서버 시작이 된 후에 리액트가 구동되로록 변경

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { server } from './mocks/node.ts';
 
-server.start();
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+server.start().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
