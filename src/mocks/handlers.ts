6fa11bec4b3a8c6963a12655a02f47b42bf81204
gogo1414 <wnstn6945@gmail.com>
2025-02-18 18:00:17 +0900

    feature: 업로드날짜, 조회수 내림차순 mock api 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,18 +1,17 @@
-import { http, HttpResponse } from 'msw';
-import { travleListResolver } from './reslovers';
+import { http } from 'msw';
+import {
+  sortTravelListByUploadDate,
+  sortTravelListByViewCount,
+  travelCourseDetailResolver,
+  travleListResolver,
+} from './reslovers';
 
 export const handlers = [
   http.get('/api/v1/travelcourses', travleListResolver),
 
-  http.get('/api/v1/travelcourses/:travelCourseId', () => {
-    return HttpResponse.json();
-  }),
+  http.get('/api/v1/travelcourses/:travelCourseId', travelCourseDetailResolver),
 
-  http.get('/api/v1/travelcourses/uploaddate', () => {
-    return HttpResponse.json();
-  }),
+  http.get('/api/v1/travelcourses/uploaddate', sortTravelListByUploadDate),
 
-  http.get('/api/v1/travelcourses/viewcount', () => {
-    return HttpResponse.json();
-  }),
+  http.get('/api/v1/travelcourses/viewcount', sortTravelListByViewCount),
 ];
--- src/mocks/reslovers.ts
+++ src/mocks/reslovers.ts
@@ -2,11 +2,11 @@ import { HttpResponse } from 'msw';
 import { mockTravelList } from './data/travelListMockData';
 import { mockTravelCourseDetails } from './data/travelCourseDetailMockData';
 
-export const travleListResolver = async ({}: { params: { countryName?: string; regionName?: string } }) => {
+export const travleListResolver = async () => {
   try {
     return HttpResponse.json(mockTravelList, { status: 200 });
   } catch (error: unknown) {
-    return HttpResponse.json({ message: 'Internal Server Error', error: error }, { status: 500 });
+    return HttpResponse.json({ message: 'Internal Server Error', error: error }, { status: 400 });
   }
 };
 
@@ -16,6 +16,28 @@ export const travelCourseDetailResolver = async ({ params }: { params: { travelC
     const response = mockTravelCourseDetails.filter((data) => data.travelCourseId === Number(travelCourseId));
     return HttpResponse.json(response, { status: 200 });
   } catch (error: unknown) {
-    return HttpResponse.json({ message: 'Internal Server Error', error: error }, { status: 500 });
+    return HttpResponse.json({ message: 'Internal Server Error', error: error }, { status: 400 });
+  }
+};
+
+export const sortTravelListByUploadDate = async () => {
+  try {
+    const response = mockTravelList.sort((a, b) => {
+      return new Date(b.uploadDate).getTime() - new Date(a.uploadDate).getTime();
+    });
+    return HttpResponse.json(response, { status: 200 });
+  } catch (error: unknown) {
+    return HttpResponse.json({ message: 'Internal Server Error', error: error }, { status: 400 });
+  }
+};
+
+export const sortTravelListByViewCount = async () => {
+  try {
+    const response = mockTravelList.sort((a, b) => {
+      return b.viewCount - a.viewCount;
+    });
+    return HttpResponse.json(response, { status: 200 });
+  } catch (error: unknown) {
+    return HttpResponse.json({ message: 'Internal Server Error', error: error }, { status: 400 });
   }
 };
