e066e2599f0e94ea92e3e1f14b7b8635e34089e6
gogo1414 <wnstn6945@gmail.com>
2025-02-18 18:14:05 +0900

    feature: 목서버 정상 응답 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import axios from 'axios';
 import GlobalStyle from './Globalstyle';
 import { useEffect } from 'react';
 
 function App() {
+  const fetchData = async () => {
+    try {
+      const requestBody = {
+        countryName: '대한민국',
+        regionName: '부산',
+      };
+      const response = axios.get('/api/v1/travelcourses', { params: requestBody });
+      return response;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const getTravelCourses = async () => {
+    const response = await fetchData();
+    console.log(response);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/v1/travelcourses');
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
+    getTravelCourses();
   }, []);
 
   return (
