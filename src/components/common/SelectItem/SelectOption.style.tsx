fd4189ef8c267d76bd9f208b098f3cf4d615c20f
songhj10207@naver.com <songhj10207@naver.com>
2025-02-20 22:58:54 +0900

    fix: 사이즈 변경 #8

--- src/components/common/SelectItem/SelectOption.style.tsx
+++ src/components/common/SelectItem/SelectOption.style.tsx
@@ -5,27 +5,27 @@ export const Option = styled.li`
   list-style: none;
   display: flex;
   flex-direction: row;
-  width: 22rem;
+  width: ${size.SIZE_023};
   height: 3.75rem;
-  padding-left: ${size.SIZE_002};
-  border-radius: 0.625rem;
+  padding-left: ${size.SIZE_003};
+  border-radius: ${size.SIZE_006};
   background-color: ${colors.WHITE};
   gap: ${size.SIZE_006};
   align-items: center;
 `;
 
 export const Image = styled.img`
-  border-radius: ${size.SIZE_002};
+  border-radius: ${size.SIZE_003};
 `;
 
 export const LocationLabelWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: ${size.SIZE_002};
+  gap: ${size.SIZE_003};
 `;
 
 export const LocationTypeText = styled.span`
   color: ${colors.GREY_500};
-  font-size: ${size.SIZE_006};
+  font-size: ${size.SIZE_007};
 `;
