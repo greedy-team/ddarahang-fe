4fde84e54c1a7003c10a5949d05e2e5eba5ec55b
gogo1414 <wnstn6945@gmail.com>
2025-02-21 15:58:09 +0900

    style: rem을 size의 상수 값으로 변경 #10

--- src/components/main/Header/Header.style.tsx
+++ src/components/main/Header/Header.style.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
+import { size } from '../../../styles/Theme';
 
 export const HeaderWrapper = styled.header`
   width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 0.625rem;
+  gap: ${size.SIZE_006}
 `;
--- src/components/main/YoutubeCard/YoutubeCard.style.tsx
+++ src/components/main/YoutubeCard/YoutubeCard.style.tsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
-import { colors } from '../../../styles/Theme';
+import { colors, size } from '../../../styles/Theme';
 
 export const YoutubeCardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 21.25rem;
-  height: 20rem;
-  margin: 0.75rem;
-  padding: 0.75rem 0.75rem 0.875rem 0.75rem;
-  border-radius: 0.875rem;
+  width: ${size.SIZE_023};
+  height: ${size.SIZE_020};
+  margin: ${size.SIZE_007};
+  padding: ${size.SIZE_021} ${size.SIZE_021} ${size.SIZE_008} ${size.SIZE_007};
+  border-radius: ${size.SIZE_008};
   border: 1px solid ${colors.GRAY_200};
-  box-shadow: 0rem 0.5rem 0.5rem rgba(0, 0, 0, 0.51);
+  box-shadow: ${size.SIZE_001} ${size.SIZE_005} ${size.SIZE_005} rgba(0, 0, 0, 0.51);
   justify-content: center;
 `;
 
@@ -18,13 +18,13 @@ export const YoutubeThumbnail = styled.img`
   width: 100%;
   aspect-ratio: 16/9;
   object-fit: cover;
-  border-radius: 0.625rem;
+  border-radius: ${size.SIZE_006};
 `;
 
 export const YoutubeTitle = styled.h2`
   font-size: 1.125rem;
-  margin-top: 1rem;
-  margin-bottom: 0.375rem;
+  margin-top: ${size.SIZE_010};
+  margin-bottom: ${size.SIZE_004};
   overflow-wrap: break-word;
   display: -webkit-box;
   -webkit-line-clamp: 2;
@@ -34,16 +34,16 @@ export const YoutubeTitle = styled.h2`
 `;
 
 export const YoutubeChannelName = styled.div`
-  font-size: 0.875rem;
-  margin: 0.25rem 0;
+  font-size: ${size.SIZE_008};
+  margin: ${size.SIZE_003} ${size.SIZE_001};
   color: ${colors.GREY_500};
 `;
 
 export const MetaData = styled.div`
   display: flex;
   width: 100%;
-  font-size: 0.875rem;
-  margin: 0.25rem 0;
+  font-size: ${size.SIZE_008};
+  margin: ${size.SIZE_003} ${size.SIZE_001};
   color: ${colors.GREY_500};
-  gap: 0.625rem;
+  gap: ${size.SIZE_006};
 `;
