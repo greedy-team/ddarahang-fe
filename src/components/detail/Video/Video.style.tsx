4abce20aa8d6ce84d918789c69b7ecd1f4a6125e
gogo1414 <wnstn6945@gmail.com>
2025-02-22 01:21:48 +0900

    style: 비디오 컨테이너 스타일 수정 #8

--- src/components/detail/Video/Video.style.tsx
+++ src/components/detail/Video/Video.style.tsx
@@ -6,18 +6,16 @@ export const VideoContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  border-color: black;
   width: 100%;
-  max-height: 720px;
-  gap: 10px;
+  min-height: 100vh;
+  gap: 30px;
 `;
 
 export const VideoWrapper = styled.div`
   position: relative;
   width: 100%;
-  max-width: 800px;
+  max-width: 720px;
   aspect-ratio: 16/9;
-  margin: 40px 0;
 `;
 
 export const Iframe = styled.iframe`
