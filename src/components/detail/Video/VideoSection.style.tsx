2474e748ad3c37028fb133bd70ff9c26738c834d
gogo1414 <wnstn6945@gmail.com>
2025-02-22 02:47:49 +0900

    feature: 상세 페이지의 비디오 섹션 구현 #18

---
+++ src/components/detail/Video/VideoSection.style.tsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import { colors, size } from '../../../styles/Theme';
+
+export const VideoSectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  max-width: 800px;
+  min-height: 100vh;
+  padding: 20px 0;
+`;
+
+export const VideoSectionHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: ${size.SIZE_010};
+  text-indent: ${size.SIZE_006};
+`;
+
+export const HeaderTitle = styled.p`
+  font-size: ${size.SIZE_010};
+  color: ${colors.BLACK};
+  font-weight: bold;
+`;
+
+export const VideoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const VideoWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 720px;
+  aspect-ratio: 16/9;
+`;
+
+export const Iframe = styled.iframe`
+  position: absolute;
+  border-radius: 15px;
+  width: 100%;
+  height: 100%;
+  border: none;
+`;
+
+export const VideoInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  max-width: 720px;
+  gap: 5px;
+  padding: 10px;
+`;
+
+export const ChannelName = styled.p`
+  font-size: ${size.SIZE_009};
+  font-weight: bold;
+  color: ${colors.GREY_600};
+`;
+
+export const VideoTitle = styled.h2`
+  font-size: ${size.SIZE_010};
+  font-weight: bold;
+  color: ${colors.BLACK};
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+export const VideoMeta = styled.p`
+  font-size: ${size.SIZE_009};
+  color: ${colors.GREY_500};
+
+  & > span {
+    margin-right: 20px;
+  }
+`;
--- src/stories/Video/Video.stories.tsx
+++ src/stories/Video/Video.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Video from '../../components/detail/Video/Video';
+import Video from '../../components/detail/Video/VideoSection';
 
 const meta = {
   title: 'Video/Video',
@@ -16,6 +16,36 @@ const meta = {
       description: '비디오 고유 ID',
       defaultValue: 'LTFAaPdvzTk',
     },
+    country: {
+      control: 'text',
+      description: '나라 이름',
+      defaultValue: '대한민국',
+    },
+    region: {
+      control: 'text',
+      description: '지역 이름',
+      defaultValue: '부산',
+    },
+    creator: {
+      control: 'text',
+      description: '유튜브 크리에이터명',
+      defaultValue: '신지',
+    },
+    title: {
+      control: 'text',
+      description: '유튜브 영상 제목',
+      defaultValue: '신지의 부산 맛집 탐방 선정 10',
+    },
+    viewCount: {
+      control: 'number',
+      description: '유튜브 영상 조회수',
+      defaultValue: 100000,
+    },
+    uploadDate: {
+      control: 'text',
+      description: '유튜브 업로드 날짜',
+      defaultValue: '2024-08-02',
+    },
   },
 } satisfies Meta<typeof Video>;
 
@@ -26,5 +56,12 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     videoId: 'yT7y8xyNHHs',
+    country: '대한민국',
+    region: '부산',
+    creator: '림리니',
+    title:
+      '[Vlog] 찐 부산 & 대구 맛집 다 있는 브이로그 | 출국 전 마지막 부산 여행 😭 | ana luisa 마직막 겨울 홀리데이 세일 🩵 | 다음 주',
+    viewCount: 18334,
+    uploadDate: '2024.05.12',
   },
 };
