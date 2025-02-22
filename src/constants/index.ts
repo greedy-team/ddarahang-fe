92b97df40461f92bf150ca1e96333827c26024e8
songhj10207@naver.com <songhj10207@naver.com>
2025-02-22 16:39:14 +0900

    refactor: 지역 상수 정리 #20

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,7 +15,7 @@ export const COUNTRYOPTION = [
   },
 ];
 
-export const REGIONOPTION = [
+export const KOREAREGIONOPTION = [
   {
     imgUrl: './image/korea.jpg',
     locationLabel: '서울',
@@ -23,9 +23,114 @@ export const REGIONOPTION = [
     isCountryOption: false,
   },
   {
-    imgUrl: './image/korea.jpg',
+    imgUrl: './image/region/jeju-do.jpg',
+    locationLabel: '제주도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/busan.jpg',
     locationLabel: '부산',
     locationType: '도시, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/incheon.jpg',
+    locationLabel: '인천',
+    locationType: '도시, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/gangwon-do.jpg',
+    locationLabel: '강원도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/daejeon.jpg',
+    locationLabel: '대전',
+    locationType: '도시, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/daegu.jpg',
+    locationLabel: '대구',
+    locationType: '도시, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/gyeonggi-do.jpg',
+    locationLabel: '경기도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/ulsan.jpg',
+    locationLabel: '울산',
+    locationType: '도시, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/gwangju.jpg',
+    locationLabel: '광주',
+    locationType: '도시, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/chungcheongbuk-do.jpg',
+    locationLabel: '충청북도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/chungcheongnam-do.jpg',
+    locationLabel: '충청남도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/gyeongsangbuk-do.jpg',
+    locationLabel: '경상북도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/gyeongsangnam-do.jpg',
+    locationLabel: '경상남도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/jeolabuk-do.jpg',
+    locationLabel: '전라북도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/jeolanam-do.jpg',
+    locationLabel: '전라남도',
+    locationType: '지역, 대한민국',
+    isCountryOption: false,
+  },
+];
+
+export const JAPANREGIONOPTION = [
+  {
+    imgUrl: './image/region/tokyo.jpg',
+    locationLabel: '도쿄',
+    locationType: '도시, 일본',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/osaka.jpg',
+    locationLabel: '오사카',
+    locationType: '도시, 일본',
+    isCountryOption: false,
+  },
+  {
+    imgUrl: './image/region/fukuoka.jpg',
+    locationLabel: '후쿠오카',
+    locationType: '도시, 일본',
     isCountryOption: false,
   },
 ];
