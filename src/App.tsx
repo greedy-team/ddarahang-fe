fda61533078de55a65443a5bf35de8d2bb1150a3
songhj10207@naver.com <songhj10207@naver.com>
2025-02-23 11:23:43 +0900

    feature: 구글 지도 API 키 발급 #26

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import GlobalStyle from './Globalstyle';
 import { useEffect } from 'react';
-import Header from './components/main/Header/Header';
 import { getTravelVideoList } from './apis/getTravelVideoList';
+import { Status, Wrapper } from '@googlemaps/react-wrapper';
+
+const render = (status: Status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <>로딩중...</>;
+    case Status.FAILURE:
+      return <>에러 발생...</>;
+    case Status.SUCCESS:
+      return <>로드 성공 !! </>;
+  }
+};
 
 function App() {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
   useEffect(() => {
     getTravelVideoList();
   }, []);
@@ -11,7 +24,10 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Wrapper
+        apiKey={apiKey}
+        render={render}
+      />
     </>
   );
 }
