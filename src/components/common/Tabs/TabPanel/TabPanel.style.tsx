7481977b93b50dcf045a9c364f7c5125147abc3e
gogo1414 <wnstn6945@gmail.com>
2025-02-25 21:37:13 +0900

    style: 여행 장소 패널 고정 크기 400px -> 200px로 변경 #34

--- src/components/common/Tabs/TabPanel/TabPanel.style.tsx
+++ src/components/common/Tabs/TabPanel/TabPanel.style.tsx
@@ -3,7 +3,7 @@ import { size } from '../../../../styles/Theme';
 
 export const Panels = styled.div`
   width: 100%;
-  height: 400px;
+  height: 200px;
   overflow-y: auto;
   padding: ${size.SIZE_008};
   display: flex;
