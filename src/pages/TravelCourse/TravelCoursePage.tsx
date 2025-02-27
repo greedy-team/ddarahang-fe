af2b118031681e14dd73c0b4dfe4486574fc41d3
gogo1414 <wnstn6945@gmail.com>
2025-02-27 12:10:33 +0900

    feature: 글로벌 컨텍스트를 활용한 나라, 지역 지정 #34

--- src/pages/TravelCourse/TravelCoursePage.tsx
+++ src/pages/TravelCourse/TravelCoursePage.tsx
@@ -7,6 +7,7 @@ import { colors } from '../../styles/Theme';
 import TravelCourse from '../../components/detail/TravelCourse/TravelCourse';
 import VideoSection from '../../components/detail/Video/VideoSection';
 import useGetTravelCourse from '../../hooks/quries/useGetTravelCourse';
+import { useSelectOptionContext } from '../../hooks/select/useSelectOptionContext';
 
 const render = (status: Status) => {
   switch (status) {
@@ -22,6 +23,7 @@ const render = (status: Status) => {
 const TravelCoursePage = () => {
   const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY_DEV;
   const { travelCourse, loading, error } = useGetTravelCourse();
+  const { selectedOption } = useSelectOptionContext();
 
   if (loading) return <p>로딩 중...</p>;
   if (error) return <p>데이터를 불러오는 중 오류가 발생했습니다.</p>;
@@ -36,8 +38,8 @@ const TravelCoursePage = () => {
         <TravelCourseContainer>
           <VideoSection
             videoId='yT7y8xyNHHs'
-            country='대한민국'
-            region='부산'
+            country={selectedOption.countryName}
+            region={selectedOption.selectedOptionLabel}
             travelCourseDetail={travelCourse}
           />
           <TravelCourse />
