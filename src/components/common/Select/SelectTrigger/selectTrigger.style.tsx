277a364633fe79b4486ab0d71b27e40451145b56
songhj10207@naver.com <songhj10207@naver.com>
2025-03-01 00:43:32 +0900

    fix: 셀렉 박스 사이즈 수정 #38

--- src/components/common/Select/SelectTrigger/selectTrigger.style.tsx
+++ src/components/common/Select/SelectTrigger/selectTrigger.style.tsx
@@ -31,6 +31,10 @@ export const SelectTriggerButton = styled.button<{ isFocus: boolean }>`
       : `
       border: none;
       `}
+
+  @media screen and (max-width: 1080px) {
+    justify-content: center;
+  }
 `;
 
 export const SelectLabel = styled.span`
@@ -46,7 +50,7 @@ export const SelectOption = styled.span`
 
 export const SelectTextWrapper = styled.div<{ isFocus: boolean }>`
   display: flex;
-  min-width: 200px;
+  width: 70%;
   flex-direction: column;
   align-items: flex-start;
 `;
