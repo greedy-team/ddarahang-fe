504c5ba03a13f7aed685072cfd794619508e1e89
songhj10207@naver.com <songhj10207@naver.com>
2025-03-07 02:50:45 +0900

    feature: 메인페이지 통합테스트

--- src/tests/main/MainPage.test.tsx
+++ src/tests/main/MainPage.test.tsx
@@ -1,59 +1,67 @@
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll, vi } from 'vitest';
 import useSubmitOption from '../../hooks/select/useSubmitOption';
 import TravelVideoList from '../../components/main/TravelVideoList/TravelVideoList';
 import { SelectOptionProvider } from '../../store/SelectOptionContext';
 import { router } from '../../router';
+import { useEffect } from 'react';
+
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
+});
+
+const rtlRender = (ui: any, options?: any) => {
+  const Wrapper = ({ children }: any) => (
+    <BrowserRouter>
+      <SelectOptionProvider>
+        <RouterProvider router={router} />
+        {children}
+      </SelectOptionProvider>
+    </BrowserRouter>
+  );
+  return render(ui, { wrapper: Wrapper, ...options });
+};
+
+const Main = () => {
+  const { getTravelVideoList, videoList } = useSubmitOption();
+
+  useEffect(() => {
+    getTravelVideoList({
+      filter: 'default',
+      countryName: '대한민국',
+      regionName: '서울',
+    });
+  }, []);
+
+  return (
+    <TravelVideoList
+      videoNumberInPage={8}
+      videoList={videoList}
+      currentPageNumber={1}
+    />
+  );
+};
 
 describe('메인 페이지', () => {
   describe('셀렉 박스 테스트 ', () => {
-    const { getTravelVideoList, videoList } = useSubmitOption();
-
     it('사용자가 선택한 지역의 데이터가 없는 경우 안내 문구가 나온다.', async () => {
-      getTravelVideoList({
-        filter: 'default',
-        countryName: '대한민국',
-        regionName: '인천',
-      });
+      rtlRender(<Main />);
 
-      render(
-        <SelectOptionProvider>
-          <TravelVideoList
-            videoNumberInPage={8}
-            videoList={videoList}
-            currentPageNumber={1}
-          />
-
-          <RouterProvider router={router} />
-        </SelectOptionProvider>,
-      );
-
-      expect(screen.getByText('여행 데이터가 존재하지 않습니다.')).toBeInTheDocument();
+      const message = await screen.findByText('여행 데이터가 존재하지 않습니다.');
+      expect(message).toBeInTheDocument();
     });
-
-    // it('나라, 지역 조회에 성공하면 영상 리스트들이 렌더링 된다.', async () => {
-    //   const Wrapper = () => {
-    //     const { getTravelVideoList, videoList } = useSubmitOption();
-
-    //     act(() => {
-    //       getTravelVideoList({
-    //         filter: 'default',
-    //         countryName: '대한민국',
-    //         regionName: '서울',
-    //       });
-    //     });
-
-    //     return <MainPage />;
-    //   };
-
-    //   render(
-    //     <SelectOptionProvider>
-    //       <Wrapper />
-    //     </SelectOptionProvider>,
-    //   );
-
-    //   expect(screen.getByText('여행 데이터가 존재하지 않습니다.')).toBeInTheDocument();
-    // });
   });
 });
