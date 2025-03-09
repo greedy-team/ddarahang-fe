f385ec6a6363c2cf7df729b5a818eac7e6c526de
songhj10207@naver.com <songhj10207@naver.com>
2025-03-09 12:13:03 +0900

    fix: 메인 페이지 테스트 코드 목 데이터로 수정 #75

--- src/tests/main/MainPage.test.tsx
+++ src/tests/main/MainPage.test.tsx
@@ -1,11 +1,7 @@
-import { MemoryRouter, RouterProvider } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen, waitFor } from '@testing-library/react';
 import { describe, it, expect, beforeAll, vi } from 'vitest';
-import useSubmitOption from '../../hooks/select/useSubmitOption';
 import TravelVideoList from '../../components/main/TravelVideoList/TravelVideoList';
-import { SelectOptionProvider } from '../../store/SelectOptionContext';
-import { router } from '../../router';
-import { useEffect } from 'react';
 
 beforeAll(() => {
   Object.defineProperty(window, 'matchMedia', {
@@ -24,32 +20,15 @@ beforeAll(() => {
 });
 
 const rtlRender = (ui: any, options?: any) => {
-  const Wrapper = ({ children }: any) => (
-    <MemoryRouter>
-      <SelectOptionProvider>
-        <RouterProvider router={router} />
-        {children}
-      </SelectOptionProvider>
-    </MemoryRouter>
-  );
+  const Wrapper = ({ children }: any) => <MemoryRouter>{children}</MemoryRouter>;
   return render(ui, { wrapper: Wrapper, ...options });
 };
 
-const Main = () => {
-  const { getTravelVideoList, videoList } = useSubmitOption();
-
-  useEffect(() => {
-    getTravelVideoList({
-      filter: 'default',
-      countryName: '대한민국',
-      regionName: '인천',
-    });
-  }, []);
-
+const CustomTravelVideoList = ({ mockVideoList }: any) => {
   return (
     <TravelVideoList
       videoNumberInPage={8}
-      videoList={videoList}
+      videoList={mockVideoList}
       currentPageNumber={1}
     />
   );
@@ -58,10 +37,31 @@ const Main = () => {
 describe('메인 페이지', () => {
   describe('셀렉 박스 테스트 ', () => {
     it('사용자가 선택한 지역의 데이터가 없는 경우 안내 문구가 나온다.', async () => {
-      rtlRender(<Main />);
+      const videoList = [
+        {
+          travelCourseId: 1,
+          title: '서울 여행 코스 추천, 데이트 코스',
+          creator: 'creator1',
+          uploadDate: '2025-03-09',
+          viewCount: 20000,
+          thumbnailUrl: 'https://example.com/video/12345',
+        },
+        {
+          travelCourseId: 2,
+          title: '부산 여행 코스 추천, 데이트 코스',
+          creator: 'creator2',
+          uploadDate: '2025-03-09',
+          viewCount: 20000,
+          thumbnailUrl: 'https://example.com/video/12345',
+        },
+      ];
 
-      const message = await screen.findByText('여행 데이터가 존재하지 않습니다.');
-      expect(message).toBeInTheDocument();
+      rtlRender(<CustomTravelVideoList mockVideoList={videoList} />);
+
+      await waitFor(() => {
+        expect(screen.getByText('서울 여행 코스 추천, 데이트 코스')).toBeInTheDocument();
+        expect(screen.getByText('부산 여행 코스 추천, 데이트 코스')).toBeInTheDocument();
+      });
     });
   });
 });
