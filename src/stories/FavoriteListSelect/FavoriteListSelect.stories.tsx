aa78e8f6c7dc319fd09cc6534574ff083f983bcc
gogo1414 <wnstn6945@gmail.com>
2025-05-01 13:43:16 +0900

    fix: FavoriteListSelect에 context provider를 주입 - position fixed 설정으로 인해 화면이 짤려 div로 해당 속성을 relative로 덮어씀

--- src/stories/FavoriteListSelect/FavoriteListSelect.stories.tsx
+++ src/stories/FavoriteListSelect/FavoriteListSelect.stories.tsx
@@ -1,18 +1,35 @@
 import { Meta, StoryObj } from '@storybook/react';
 import FavoriteListSelect from '../../components/detail/TravelCourseFavorite/FavoriteListSelect';
+import { SelectFavoriteListContext } from '../../store/SelectFavoriteListContext';
+import { useState } from 'react';
 
 const meta: Meta<typeof FavoriteListSelect> = {
-  title: 'Components/TravelListSelector',
+  title: 'Components/FavoriteListSelect',
   component: FavoriteListSelect,
   parameters: {
-    layout: 'centered',
+    layout: 'padded',
   },
-};
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => {
+      const [isOpen, setIsOpen] = useState(true); // 항상 열림
+      return (
+        <SelectFavoriteListContext.Provider
+          value={{ isFavoriteListSelectOpen: isOpen, setIsFavoriteListSelectOpen: setIsOpen }}
+        >
+          <div style={{ position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden' }}>
+            <Story />
+          </div>
+        </SelectFavoriteListContext.Provider>
+      );
+    },
+  ],
+} satisfies Meta<typeof FavoriteListSelect>;
 
 export default meta;
 
-type Story = StoryObj<typeof FavoriteListSelect>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: () => <FavoriteListSelect />,
+  args: {},
 };
